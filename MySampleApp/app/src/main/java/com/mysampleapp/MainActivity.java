b38db262f01a8058db3131df35d3ae9ad6ab6702
Min Jung Kim <mmkim801@berkeley.edu>
2017-06-30 22:58:12 -0700

    Implemented DatabaseHandler
    DatabaseHandler fixed
    
    DatabaseHandler fixed 2

--- MySampleApp/app/src/main/java/com/mysampleapp/MainActivity.java
+++ MySampleApp/app/src/main/java/com/mysampleapp/MainActivity.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
