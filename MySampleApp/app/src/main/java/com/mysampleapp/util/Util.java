94e8f9219aa1bec50c057c19fe80c447fa1a009b
jongleeee <lee.jongyun13@gmail.com>
2018-05-31 21:22:14 -0700

    Added Util file to expose list of subscribeable channels (#32)

---
+++ MySampleApp/app/src/main/java/com/mysampleapp/util/Util.java
@@ -0,0 +1,12 @@
+package com.mysampleapp.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Util {
+
+    public static List<String> getAllChannels() {
+        return Arrays.asList("죠이플 창", "교회 소식 (전체 공지)", "카리스마 대학부", "카이로스 청년부", "남성 기도회", "월요 여성 중보기도 모임", "여성 커피브레이크", "교육부", "찬양팀", "여름 선교", "겨울 선교", "목자 모임", "사역부장 모임");
+    }
+
+}
