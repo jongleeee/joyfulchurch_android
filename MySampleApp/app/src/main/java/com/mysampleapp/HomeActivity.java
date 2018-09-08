af1ae925500862baabdc9a33c3c14e74389fa73a
jongleee <jonglee@berkeley.edu>
2018-09-08 11:15:52 -0700

    Added backbone for notification system

--- MySampleApp/app/src/main/java/com/mysampleapp/HomeActivity.java
+++ MySampleApp/app/src/main/java/com/mysampleapp/HomeActivity.java
@@ -1,9 +1,15 @@
 package com.mysampleapp;
 
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -13,6 +19,8 @@ import com.mysampleapp.util.Announcement;
 import java.lang.annotation.Inherited;
 
 public class HomeActivity extends AppCompatActivity {
+    /** Class name for log messages. */
+    private static final String LOG_TAG = HomeActivity.class.getSimpleName();
 
     ConstraintLayout SermonHeader;
     ConstraintLayout AnnouncementHeader;
@@ -48,6 +56,39 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), InformationActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // register notification receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(notificationReceiver,
+                new IntentFilter(PushListenerService.ACTION_SNS_NOTIFICATION));
+    }
+
+    private final BroadcastReceiver notificationReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(LOG_TAG, "Received notification from local broadcast. Display it in a dialog.");
+
+            Bundle data = intent.getBundleExtra(PushListenerService.INTENT_SNS_NOTIFICATION_DATA);
+            String message = PushListenerService.getMessage(data);
+
+            new AlertDialog.Builder(HomeActivity.this)
+                    .setTitle(R.string.push_demo_title)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // unregister notification receiver
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(notificationReceiver);
     }
 
     @Override
---
+++ MySampleApp/app/src/main/java/com/mysampleapp/PushListenerService.java
@@ -0,0 +1,123 @@
+package com.mysampleapp;
+
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+import java.util.List;
+
+/**
+ * A service that listens to GCM notifications.
+ */
+public class PushListenerService extends GcmListenerService {
+
+    private static final String LOG_TAG = PushListenerService.class.getSimpleName();
+
+    // Intent action used in local broadcast
+    public static final String ACTION_SNS_NOTIFICATION = "sns-notification";
+    // Intent keys
+    public static final String INTENT_SNS_NOTIFICATION_FROM = "from";
+    public static final String INTENT_SNS_NOTIFICATION_DATA = "data";
+
+    public static final String CHANNEL_ID = "joyfulchurch_channel_01";
+
+    /**
+     * Helper method to extract SNS message from bundle.
+     *
+     * @param data bundle
+     * @return message string from SNS push notification
+     */
+    public static String getMessage(Bundle data) {
+        // If a push notification is sent as plain text, then the message appears in "default".
+        // Otherwise it's in the "message" for JSON format.
+        return data.containsKey("default") ? data.getString("default") : data.getString(
+                "message", "");
+    }
+
+    private static boolean isForeground(Context context) {
+        // Gets a list of running processes.
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> tasks = am.getRunningAppProcesses();
+
+        // On some versions of android the first item in the list is what runs in the foreground,
+        // but this is not true on all versions.  Check the process importance to see if the app
+        // is in the foreground.
+        final String packageName = context.getPackageName();
+        for (ActivityManager.RunningAppProcessInfo appProcess : tasks) {
+            if (ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND == appProcess.importance
+                    && packageName.equals(appProcess.processName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void displayNotification(final String message) {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.setFlags(
+                Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        int requestID = (int) System.currentTimeMillis();
+        PendingIntent contentIntent = PendingIntent.getActivity(this, requestID, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Display a notification with an icon, message as content, and default sound. It also
+        // opens the app when the notification is clicked.
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this).setSmallIcon(
+                R.mipmap.push)
+                .setContentTitle(getString(R.string.push_demo_title))
+                .setContentText(message)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setAutoCancel(true)
+                .setContentIntent(contentIntent)
+                .setChannelId(CHANNEL_ID);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, "joyfulchurch_announcement", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        notificationManager.notify(0, builder.build());
+    }
+
+    private void broadcast(final String from, final Bundle data) {
+        Intent intent = new Intent(ACTION_SNS_NOTIFICATION);
+        intent.putExtra(INTENT_SNS_NOTIFICATION_FROM, from);
+        intent.putExtra(INTENT_SNS_NOTIFICATION_DATA, data);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    /**
+     * Called when message is received.
+     *
+     * @param from SenderID of the sender.
+     * @param data Data bundle containing message data as key/value pairs. For Set of keys use
+     * data.keySet().
+     */
+    @Override
+    public void onMessageReceived(final String from, final Bundle data) {
+        String message = getMessage(data);
+        Log.d(LOG_TAG, "From: " + from);
+        Log.d(LOG_TAG, "Message: " + message);
+        // Display a notification in the notification center if the app is in the background.
+        // Otherwise, send a local broadcast to the app and let the app handle it.
+        if (isForeground(this)) {
+            // broadcast notification
+            broadcast(from, data);
+        } else {
+            displayNotification(message);
+        }
+    }
+}
