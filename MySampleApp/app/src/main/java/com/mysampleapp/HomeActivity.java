d061f3b0d0376b5b4bc1182da660e957e43e13c5
jongleee <jonglee@berkeley.edu>
2018-09-29 12:27:30 -0700

    Updated fonts and splash image and some extra UI stuff

--- MySampleApp/app/src/main/java/com/mysampleapp/HomeActivity.java
+++ MySampleApp/app/src/main/java/com/mysampleapp/HomeActivity.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Typeface;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
